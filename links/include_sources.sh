a10b530c5d0dc7e2b5912dfabd2d44af5634ef97
dev-allinappli <dev@all-in-appli.com>
2025-05-05 15:39:59 +0200

    [ADD] colors

--- links/include_sources.sh
+++ links/include_sources.sh
@@ -13,6 +13,16 @@ fi
 export PATH="$PATH:/etc/my_common/bin:/etc/my_common/officialbin"
 export CUSTOM_DIR="/etc/my_common"
 export racine="/var/www/vhosts"
+export GREEN='\e[1;38;5;2m'
+export BLACK='\e[1;90m'
+export RED='\e[1;31m'
+export YELLOW='\e[1;33m'
+export BLUE='\e[1;34m'
+export PURPLE='\e[1;35m'
+export CYAN='\e[1;36m'
+export WHITE='\e[97m'
+export GREY='\e[90m'
+export NC='\e[0m'
 
 for ele in $(ls /etc/my_common/sources); do
     fullpath="/etc/my_common/sources/$ele"
