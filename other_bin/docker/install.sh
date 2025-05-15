55f9a3564a63c8a8804536a591a8aa6b0fcac3fe
dev-allinappli <dev@all-in-appli.com>
2025-05-15 11:25:30 +0200

    [MOD] installation of drush

--- other_bin/docker/install.sh
+++ other_bin/docker/install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-sudo dnf install docker
+test -e /etc/my_common || {
+	mkdir /etc/my_common
+}
+
+test -e /etc/my_common/officialbin || {
+	mkdir /etc/my_common/officialbin
+}
+
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /etc/my_common/officialbin/docker-compose
+chmod +x /etc/my_common/officialbin/docker-compose
+
